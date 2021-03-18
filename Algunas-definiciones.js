079da2897f6e6388905020ce6a0973967d3641df
Majo-code <majolet@gmail.com>
2021-03-18 15:57:14 -0300

    Ejercicios 2 y 3

---
+++ Algunas-definiciones.js
@@ -0,0 +1,6 @@
+
+//* Variables = es una "cajita" con un nombre a la cual se le asigna un valor y ese valor estará presente en todos lados (de la misma funcion) donde se utilice esa "cajita"
+//* Strings = tipo de dato que soporta letras, numeros, caracteres, etc.
+//* Funciones (argumentos, `return`) = una funcion es esa seccion en el código que hace un "trabajo" en particular. A la funcion le podes pasar argumentos, que lo podrá utilizar para el trabajo que tiene que hacer y te devuelve algo que lo podes usar para otra sección del código
+//* Declaraciones `if` = if es un comportamiento que puede tener ese "trabajo". Se le puede indicar que si pasa algo haga tal cosa y sino haga tal otra
+//* Valores booleanos (`true`, `false`) = es un tipo de dato que solo tiene esos dos valores, si se cumple una condicion quedará en true, sino quedará en false. Es como una perilla de luz, si esta encendida el valor esta en true, si esta apagada el valor esta en false
